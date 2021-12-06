113fc1404fd1dca6eb386d63a88f77842d77d320
Wuwime <wallovas@gmail.com>
2021-12-06 13:26:23 +0100

    updated Id to PlayerId,

--- DumDum/DumDum/Models/Entities/Player.cs
+++ DumDum/DumDum/Models/Entities/Player.cs
@@ -2,7 +2,7 @@ namespace DumDum.Models.Entities
 {
     public class Player
     {
-        public int Id { get; set; }
+        public int PlayerId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public int KingdomId { get; set; }
