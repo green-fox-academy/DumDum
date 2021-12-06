d04f5420b2411ad84f49e2c24b32f367d3e89894
unknown <mladenstupar@gmail.com>
2021-12-06 11:32:38 +0100

    Added empty solution

---
+++ DumDum/DumDum/Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DumDum.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
