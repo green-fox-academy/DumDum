738f4d9df40c7c39e7fd1ec40f871ba3cbe89edd
Silvia Walla <74986270+Wuwime@users.noreply.github.com>
2021-12-09 14:53:21 +0100

    deleted useless dictionary

--- DumDum/DumDum/Services/ResourceService.cs
+++ DumDum/DumDum/Services/ResourceService.cs
@@ -11,13 +11,5 @@ namespace DumDum.Services
         {
             DbContext = dbContex;
         }
-
-        public Dictionary<string, int> AddToDictionary(int coordinateX, int coordinateY)
-        {
-            Dictionary<string, int> dictionaryToReturn = new Dictionary<string, int>();
-            dictionaryToReturn.Add("CoordinateX", coordinateX);
-            dictionaryToReturn.Add("CoordinateY", coordinateY);
-            return dictionaryToReturn;
-        }
     }
 }
