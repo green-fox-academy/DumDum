8ce5084523a25ff1f52cbb3cf34884e760deab82
MatUrb2 <matejmr.urbanek@gmail.com>
2021-12-10 13:17:12 +0100

    Modified buildingmodel

--- DumDum/DumDum/Models/Entities/Building.cs
+++ DumDum/DumDum/Models/Entities/Building.cs
@@ -10,8 +10,8 @@ namespace DumDum.Models.Entities
         public int BuildingId { get; set; }
         public string BuildingType { get; set; }
         public int Level { get; set; }
-        public long Started_at { get; set; }
-        public long Finished_at { get; set; }
+        public long StartedAt { get; set; }
+        public long FinishedAt { get; set; }
         public Kingdom Kingdom { get; set; }
         public int KingdomId { get; set; }
     }
