36c24930235861cb01e16ffbfb4740b2c69d21a9
Marek Holba <marekholba@gmail.com>
2021-12-12 17:12:26 +0100

    add Json response

---
+++ DumDum/DumDum/Models/JsonEntities/ListResponse.cs
@@ -0,0 +1,13 @@
+﻿using DumDum.Models.Entities;
+
+namespace DumDum.Models.JsonEntities
+{
+    public class ListResponse
+    {
+        public int KingdomId { get; set; }
+        public string KingdomName { get; set; }
+        public string Ruler { get; set; }
+        public int Population { get; set; }
+        public Location Location { get; set; }
+    }
+}
