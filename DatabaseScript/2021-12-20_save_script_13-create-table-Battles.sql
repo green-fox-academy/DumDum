2864e8c38ea38bbd544327432811a7d3dc5efc22
Wuwime <wallovas@gmail.com>
2021-12-26 19:05:36 +0100

    created tables for battles

---
+++ DatabaseScript/2021-12-20_save_script_13-create-table-Battles.sql
@@ -0,0 +1,23 @@
+use DumDum;
+Create table Battles(
+    BattleId int not null auto_increment,
+    AttackerName varchar (255),
+    Target varchar (255),
+    BattleType varchar (255),
+    ResolutionTime bigint,
+    TimeToStartTheBattle bigint,
+    primary key (BattleId) 
+);
+Create table Attackers(
+    AttackerId int not null auto_increment,
+    AttackerName varchar (255),
+    BattleId int not null,
+    primary key (AttackerId) 
+);
+Create table Defenders(
+    DefenderId int not null auto_increment,
+    DefenderName varchar (255),
+    BattleId int not null,
+    primary key (DefenderId) 
+);
+
