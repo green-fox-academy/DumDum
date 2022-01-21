0ac0e25d51348509df8958b05551ddd9d58cd1e4
stmlad <81235340+stmlad@users.noreply.github.com>
2022-01-21 14:23:59 +0100

    DB cleanup

---
+++ DatabaseScript/2022-01-21_update-tables-cleanup.sql
@@ -0,0 +1,9 @@
+USE dumdum;
+ALTER TABLE dumdum.buildings DROP COLUMN Hp;
+ALTER TABLE dumdum.buildinglevels DROP COLUMN ResearchTime;
+ALTER TABLE dumdum.buildinglevels DROP COLUMN MaxStorage;
+ALTER TABLE dumdum.buildinglevels DROP COLUMN Defense;
+ALTER TABLE dumdum.buildinglevels DROP COLUMN DefBoost;
+ALTER TABLE dumdum.trooplevel DROP COLUMN HP;
+ALTER TABLE dumdum.trooplevel DROP COLUMN CarryCap;
+ALTER TABLE dumdum.trooplevel DROP COLUMN SpecialSkills;
--- DumDum/DumDum/Models/Entities/TroopLevel.cs
+++ DumDum/DumDum/Models/Entities/TroopLevel.cs
@@ -9,14 +9,11 @@ namespace DumDum.Models.Entities
         [Key]
         public int TroopLevelId { get; set; }
         public int Level { get; set; }
-        public int HP { get; set; }
         public double Attack { get; set; }
         public double Defence { get; set; }
         public double Speed { get; set; }
         public double Consumption { get; set; }
-        public int CarryCap { get; set; }
         public int Cost { get; set; }
-        public int SpecialSkills { get; set; }
         public int ConstTime { get; set; }
         public int TroopTypeId { get; set; }
         [NotMapped]public TroopTypes TroopType { get; set; }
