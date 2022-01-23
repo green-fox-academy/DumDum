e5f53f858f1f8dee24eae26d2922a5ff26398826
stmlad <81235340+stmlad@users.noreply.github.com>
2022-01-23 21:37:32 +0100

    updated program.cs

--- DumDum/DumDum/Program.cs
+++ DumDum/DumDum/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using Serilog;
 using Serilog.Formatting.Compact;
 using System;
@@ -13,7 +15,7 @@ namespace DumDum
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
 
@@ -37,6 +39,13 @@ namespace DumDum
             {
                 Log.CloseAndFlush();
             }
+
+
+            var apiKey = Environment.GetEnvironmentVariable("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");
+            var client = new SendGridClient(apiKey);
+            var queryParams = @"{'limit': 100}";
+            var response = await client.RequestAsync(method: SendGridClient.Method.GET, urlPath: "suppression/bounces",
+            queryParams: queryParams).ConfigureAwait(false);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
