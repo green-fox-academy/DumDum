68a133c01002083cabc8b978bb7e46bd4c0e3a91
stmlad <81235340+stmlad@users.noreply.github.com>
2022-01-23 23:39:28 +0100

    back to sendgrid

--- DumDum/DumDum/Services/AuthenticateService.cs
+++ DumDum/DumDum/Services/AuthenticateService.cs
@@ -98,6 +98,31 @@ namespace DumDum.Services
             }
         }
 
+        //public async Task SendAccountVerificationEmail(Player player)
+        //{
+        //    MailMessage mail = new MailMessage();
+
+        //    mail.From = new MailAddress("dumdumnya@gmail.com", "DumDum");
+        //    mail.To.Add(new MailAddress(player.Email));
+
+        //    mail.Subject = "Account verification";
+        //    mail.IsBodyHtml = true;
+        //    mail.Body =
+        //        $"Welcome to DumDum, {player.Username}!\n The last step of registration is email verification.\n " +
+        //        $"All you need is to click <a href=\"http://dumdumdumdum.azurewebsites.net/emailAuthenticated/{player.PlayerId}?hash={player.Password}\">this link</a>";
+        //    mail.Priority = MailPriority.High;
+        //   // var loginInfo = new NetworkCredential("dumdumnya@gmail.com", "dumdumcatcatcat");
+        //    var loginInfo = CredentialCache.DefaultNetworkCredentials;
+
+
+        //    SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+
+        //    smtp.Credentials = loginInfo;
+        //    smtp.UseDefaultCredentials = true;
+        //    smtp.EnableSsl = true;
+        //    smtp.Send(mail);
+        //}
+
         public async Task SendAccountVerificationEmail(Player player)
         {
             MailMessage mail = new MailMessage();
@@ -111,19 +136,14 @@ namespace DumDum.Services
                 $"Welcome to DumDum, {player.Username}!\n The last step of registration is email verification.\n " +
                 $"All you need is to click <a href=\"http://dumdumdumdum.azurewebsites.net/emailAuthenticated/{player.PlayerId}?hash={player.Password}\">this link</a>";
             mail.Priority = MailPriority.High;
-           // var loginInfo = new NetworkCredential("dumdumnya@gmail.com", "dumdumcatcatcat");
-            var loginInfo = CredentialCache.DefaultNetworkCredentials;
+            var loginInfo = new NetworkCredential("apikey", "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");
 
-
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            SmtpClient smtp = new SmtpClient("smtp.sendgrid.net", 587);
 
             smtp.Credentials = loginInfo;
-            smtp.UseDefaultCredentials = true;
             smtp.EnableSsl = true;
             smtp.Send(mail);
         }
-
-
 
         public async Task SendPasswordResetEmail(Player player)
         {
